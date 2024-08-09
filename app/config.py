f24faaa2c9bbdff177655aaf7651e6558b7ffca9
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-08-09 23:14:39 +0200

    flask configurations

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://derrickuser:123@localhost/derricktest'
     SQLALCHEMY_TRACK_MODIFICATIONS = False,
-    SECRET_KEY = 'AI WILL RULE THE DAY'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'AI'
